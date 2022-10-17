fa9b2ee5b878b866d65474f6e357753b52d29307
Javier Ramirez <jramir11@bancoestado.cl>
2022-10-17 14:17:14 -0300

    commit inicial

---
+++ EventoOnClick.js
@@ -0,0 +1,12 @@
+function changeBtnLogin(element) {
+        element.innerText="Logout";
+}
+
+function removeBtnAdd(element) {
+         element.remove();
+ }
+
+ function btn13likesIzq11(element) {
+    alert("Ninja was liked");
+ }
+ 
